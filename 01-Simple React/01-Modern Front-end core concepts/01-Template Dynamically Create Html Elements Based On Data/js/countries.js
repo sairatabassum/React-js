cbce53cb2094f0e7b82ad567a404af2ab9b20718
sairatabassum <sairatabassum386@gmail.com>
2023-03-13 18:09:05 +0600

    commit

---
+++ 01-Simple React/01-Modern Front-end core concepts/01-Template Dynamically Create Html Elements Based On Data/js/countries.js
@@ -0,0 +1,25 @@
+const loadCountries = () => {
+  fetch("https://restcountries.com/v3.1/all")
+    .then((res) => res.json())
+    .then((data) => displayCountries(data));
+};
+
+const displayCountries = (countries) => {
+  console.log(countries);
+  const CountriesHTML = countries.map((country) => getCountryHTML(country));
+  //   console.log(CountriesHTML);
+
+  const container = document.getElementById("countries");
+  container.innerHTML = CountriesHTML.join(" ");
+};
+
+const getCountryHTML = (country) => {
+  return `
+    <div class="country">
+    <h2>${country.name.common}</h2>
+    <img src="${country.flags.png}">
+    </div>
+    `;
+};
+
+loadCountries();
