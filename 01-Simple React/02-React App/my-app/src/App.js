6a92923df46b09bba86fbff1c1a5774af1908af0
sairatabassum <sairatabassum386@gmail.com>
2023-03-14 19:44:22 +0600

    commit

--- 01-Simple React/02-React App/my-app/src/App.js
+++ 01-Simple React/02-React App/my-app/src/App.js
@@ -1,11 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+const number = 555;
+const singer = { name: "DR.Mahfuz", job: "singer" };
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>JSX</h1>
+        <div className="container">
+          <h3>Hello dude. React how are you</h3>
+        </div>
+        <div className="music">
+          <p>Name: {222 + number}</p>
+          <p>
+            Name: {singer.name} job: {singer.job}
+          </p>
+        </div>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
