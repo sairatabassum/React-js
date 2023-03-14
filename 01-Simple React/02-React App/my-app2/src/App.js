09c5465f08a0cba43ff0cfc92a8e9add6df0cbb2
sairatabassum <sairatabassum386@gmail.com>
2023-03-15 00:20:14 +0600

    commit

--- 01-Simple React/02-React App/my-app2/src/App.js
+++ 01-Simple React/02-React App/my-app2/src/App.js
@@ -1,17 +1,33 @@
 import logo from "./logo.svg";
 import "./App.css";
 
+const singers = [
+  { name: "Dr.mahfuz", job: "singer" },
+  { name: "Eva Tahman", job: "singer2" },
+  { name: "Agun", job: "sopno" },
+  { name: "Shuvro", job: "singer" },
+];
+
 function App() {
-  const nayoks = ["Rubel", "BappaRaz", "Kuber"];
+  const nayoks = ["Rubel", "BappaRaz", "Kuber", "Koti"];
+  const nayika = ["moushumi", "cheka", "kopila"];
   return (
     <div className="App">
       {/* <Person name="saira" nayika="moushumi"></Person>
       <Person name="tabassum" nayika="cheka"></Person>
       <Person name="Kuber" nayika="kopila"></Person> */}
 
-      <Person name={nayoks[0]}></Person>
-      <Person name={nayoks[1]}></Person>
-      <Person name={nayoks[2]}></Person>
+      {/* <Person name={nayoks[0]} nayika={nayika[0]}></Person>
+      <Person name={nayoks[1]} nayika={nayika[1]}></Person>
+      <Person name={nayoks[2]} nayika={nayika[2]}></Person> */}
+
+      {/* {nayoks.map((nayok) => (
+        <Person name={nayok}></Person>
+      ))} */}
+
+      {singers.map((singer) => (
+        <Person name={singer.name} job={singer.job}></Person>
+      ))}
 
       <h5>New component</h5>
       <Friend></Friend>
@@ -25,7 +41,8 @@ function Person(props) {
   return (
     <div className="person">
       <h1>{props.name}</h1>
-      <p>Nayika: {props.nayika}</p>
+      {/* <p>Nayika: {props.nayika}</p> */}
+      <p>{props.job}</p>
     </div>
   );
 }
