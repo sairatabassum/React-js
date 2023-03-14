967d7ead3eb4a93b40c2b0e87f3235f3984c7c1f
sairatabassum <sairatabassum386@gmail.com>
2023-03-14 23:10:19 +0600

    commit

--- 01-Simple React/02-React App/my-app2/src/App.js
+++ 01-Simple React/02-React App/my-app2/src/App.js
@@ -2,11 +2,17 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  const nayoks = ["Rubel", "BappaRaz", "Kuber"];
   return (
     <div className="App">
-      <Person name="saira" nayika="moushumi"></Person>
+      {/* <Person name="saira" nayika="moushumi"></Person>
       <Person name="tabassum" nayika="cheka"></Person>
-      <Person name="Kuber" nayika="kopila"></Person>
+      <Person name="Kuber" nayika="kopila"></Person> */}
+
+      <Person name={nayoks[0]}></Person>
+      <Person name={nayoks[1]}></Person>
+      <Person name={nayoks[2]}></Person>
+
       <h5>New component</h5>
       <Friend></Friend>
       <Friend></Friend>
