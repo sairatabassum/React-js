e0f880cd8598cb3066b186f60f9dc29121b3a2af
sairatabassum <sairatabassum386@gmail.com>
2023-03-15 18:43:19 +0600

    commit

--- 01-Simple React/02-React App/react-rest-countries/src/App.js
+++ 01-Simple React/02-React App/react-rest-countries/src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <LoadCountries></LoadCountries>
+    </div>
+  );
+}
+
+function LoadCountries() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    fetch("https://restcountries.com/v3.1/all")
+      .then((res) => res.json())
+      .then((data) => setCountries(data));
+  }, []);
+
+  return (
+    <div>
+      <h1>Visiting Every Country of the world</h1>
+      <h3>Available Countires: {countries.length} </h3>
+
+      {countries.map((country) => console.log(country.region))}
     </div>
   );
 }
