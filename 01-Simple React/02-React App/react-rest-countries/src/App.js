8d348acf2514ae6d9360fe1c6f85ab2ffea31ace
sairatabassum <sairatabassum386@gmail.com>
2023-03-16 00:35:31 +0600

    commit

--- 01-Simple React/02-React App/react-rest-countries/src/App.js
+++ 01-Simple React/02-React App/react-rest-countries/src/App.js
@@ -3,44 +3,40 @@ import "./App.css";
 import { useState, useEffect } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <LoadCountries></LoadCountries>
-    </div>
-  );
+  return <div className="App">] </div>;
 }
 
-function LoadCountries() {
-  const [countries, setCountries] = useState([]);
+// function LoadCountries() {
+//   const [countries, setCountries] = useState([]);
 
-  useEffect(() => {
-    fetch("https://restcountries.com/v3.1/all")
-      .then((res) => res.json())
-      .then((data) => setCountries(data));
-  }, []);
+//   useEffect(() => {
+//     fetch("https://restcountries.com/v3.1/all")
+//       .then((res) => res.json())
+//       .then((data) => setCountries(data));
+//   }, []);
 
-  return (
-    <div>
-      <h1>Visiting Every Country of the world</h1>
-      <h3>Available Countires: {countries.length} </h3>
+//   return (
+//     <div>
+//       <h1>Visiting Every Country of the world</h1>
+//       <h3>Available Countires: {countries.length} </h3>
 
-      {countries.map((country) => (
-        <Country
-          name={country.name.common}
-          population={country.population}
-        ></Country>
-      ))}
-    </div>
-  );
-}
+//       {countries.map((country) => (
+//         <Country
+//           name={country.name.common}
+//           population={country.population}
+//         ></Country>
+//       ))}
+//     </div>
+//   );
+// }
 
-function Country(props) {
-  return (
-    <div className="country">
-      <h2>Name: {props.name}</h2>
-      <p>Population: {props.population}</p>
-    </div>
-  );
-}
+// function Country(props) {
+//   return (
+//     <div className="country">
+//       <h2>Name: {props.name}</h2>
+//       <p>Population: {props.population}</p>
+//     </div>
+//   );
+// }
 
 export default App;
