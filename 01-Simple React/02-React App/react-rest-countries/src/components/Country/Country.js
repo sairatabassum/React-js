665990c2acbefe68072b64d66c7e356dd6573936
sairatabassum <sairatabassum386@gmail.com>
2023-03-17 23:25:45 +0600

    commit

--- 01-Simple React/02-React App/react-rest-countries/src/components/Country/Country.js
+++ 01-Simple React/02-React App/react-rest-countries/src/components/Country/Country.js
@@ -3,15 +3,25 @@ import "./Country.css";
 
 const Country = (props) => {
   // console.log(props);
+  const { area, region, population, name } = props.country;
   return (
     <div className="country">
-      <h2>Country Name: {props.country.name.common}</h2>
+      {/* <h2>Country Name: {props.country.name.common}</h2>
       <p>population={props.country.population}</p>
       <p>
         <small>Region: {props.country.region}</small>
       </p>
       <p>
         <small>Area={props.country.area}</small>
+      </p> */}
+
+      <h2>Country Name: {name.common}</h2>
+      <p>population={population}</p>
+      <p>
+        <small>Region: {region}</small>
+      </p>
+      <p>
+        <small>Area={area}</small>
       </p>
     </div>
   );
