ce0191b47f33ae8e2ea4fa38a9143773628cd8ce
sairatabassum <sairatabassum386@gmail.com>
2023-03-17 19:30:53 +0600

    commit

--- 01-Simple React/02-React App/react-rest-countries/src/components/Countries/Countries.js
+++ 01-Simple React/02-React App/react-rest-countries/src/components/Countries/Countries.js
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const Countries = () => {
-  return <div></div>;
+  const [countries, setCountries] = useState();
+
+  useEffect(() => {
+    fetch("https://restcountries.com/v3.1/all")
+      .then((res) => res.json())
+      .then((data) => setCountries(data));
+  }, []);
+
+  return (
+    <div>
+      <h1>Hello from countries: {countries.length}</h1>
+    
+    {
+        countries.map(country=>)
+    }
+    </div>
+  );
 };
 
 export default Countries;
