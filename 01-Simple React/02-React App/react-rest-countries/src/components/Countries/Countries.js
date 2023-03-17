12249bfa28bd9893760ca69af3f94723fd11d390
sairatabassum <sairatabassum386@gmail.com>
2023-03-17 23:20:36 +0600

    commit

--- 01-Simple React/02-React App/react-rest-countries/src/components/Countries/Countries.js
+++ 01-Simple React/02-React App/react-rest-countries/src/components/Countries/Countries.js
@@ -14,11 +14,7 @@ const Countries = () => {
     <div>
       <h1>Hello from countries: {countries.length}</h1>
       {countries.map((country) => (
-        <Country
-          name={country.name.common}
-          population={country.population}
-          area={country.area}
-        ></Country>
+        <Country country={country}></Country>
       ))}
     </div>
   );
--- 01-Simple React/02-React App/react-rest-countries/src/components/Country/Country.js
+++ 01-Simple React/02-React App/react-rest-countries/src/components/Country/Country.js
@@ -2,12 +2,16 @@ import React from "react";
 import "./Country.css";
 
 const Country = (props) => {
+  // console.log(props);
   return (
     <div className="country">
-      <h2>Country Name: {props.name}</h2>
-      <p>population={props.population}</p>
+      <h2>Country Name: {props.country.name.common}</h2>
+      <p>population={props.country.population}</p>
       <p>
-        <small>Area={props.area}</small>
+        <small>Region: {props.country.region}</small>
+      </p>
+      <p>
+        <small>Area={props.country.area}</small>
       </p>
     </div>
   );
