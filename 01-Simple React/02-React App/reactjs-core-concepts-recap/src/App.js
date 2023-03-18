2f6d1e44a0baac4fb533016a2129b6d3be84f077
sairatabassum <sairatabassum386@gmail.com>
2023-03-18 15:56:20 +0600

    commit

--- 01-Simple React/02-React App/reactjs-core-concepts-recap/src/App.js
+++ 01-Simple React/02-React App/reactjs-core-concepts-recap/src/App.js
@@ -2,7 +2,28 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <District name="NeoCalifornia" special="vibag"></District>
+      <District name="Brahomnbaria" special="jodha akbar"></District>
+      <District name="Sumilla" special="Moyna and moti"></District>
+    </div>
+  );
+}
+
+const districtStyle = {
+  backgroundColor: "yellow",
+  margin: "20px",
+  borderRadius: "20px",
+};
+
+function District(props) {
+  return (
+    <div style={districtStyle}>
+      <h2>Name:{props.name}</h2>
+      <p>Speciality: {props.special} </p>
+    </div>
+  );
 }
 
 export default App;
