f5a543bb4130ebc25849d5dfb4c1fe1566893340
sairatabassum <sairatabassum386@gmail.com>
2023-03-30 00:55:26 +0600

    commit

--- 01-Simple React/03-Modules and Data Storage/react-storage-and-handler/src/Components/Cosmetics/Cosmetics.js
+++ 01-Simple React/03-Modules and Data Storage/react-storage-and-handler/src/Components/Cosmetics/Cosmetics.js
@@ -1,9 +1,14 @@
 import React from "react";
+import add from "../../Utilities/calculate";
 
 const Cosmetics = () => {
+  const first = 55;
+  const second = 66;
+  const total = add(first, second);
   return (
     <div>
       <h1>Welcome to my cosmetics</h1>
+      <p>Total: {total}</p>
     </div>
   );
 };
