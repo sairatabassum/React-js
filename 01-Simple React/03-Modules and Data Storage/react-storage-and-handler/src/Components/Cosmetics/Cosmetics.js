409309def52c02dfc32028e56c4b69499f5312d8
sairatabassum <sairatabassum386@gmail.com>
2023-03-30 03:17:11 +0600

    commit

--- 01-Simple React/03-Modules and Data Storage/react-storage-and-handler/src/Components/Cosmetics/Cosmetics.js
+++ 01-Simple React/03-Modules and Data Storage/react-storage-and-handler/src/Components/Cosmetics/Cosmetics.js
@@ -1,15 +1,51 @@
 import React from "react";
 import { add } from "../../Utilities/calculate";
+import Cosmetic from "../Cosmetic/Cosmetic";
 // import add from "../../Utilities/calculate";
 
 const Cosmetics = () => {
   const first = 55;
   const second = 66;
   const total = add(first, second);
+  const cosmetics = [
+    {
+      id: "6424a9ee9bf07614eda1eba5",
+      price: 99,
+      name: "Beryl Riley",
+    },
+    {
+      id: "6424a9ee9e1d64431657a7c3",
+      price: 600,
+      name: "Ernestine Rios",
+    },
+    {
+      id: "6424a9ee4150ef04ad2c764f",
+      price: 464,
+      name: "Houston Farrell",
+    },
+    {
+      id: "6424a9ee188543b5de4436c1",
+      price: 68,
+      name: "Knox Hernandez",
+    },
+    {
+      id: "6424a9eeb481060964705557",
+      price: 42,
+      name: "Gena Haney",
+    },
+    {
+      id: "6424a9eeda63d5679e7d7b98",
+      price: 203,
+      name: "Candace Davis",
+    },
+  ];
   return (
     <div>
       <h1>Welcome to my cosmetics</h1>
       <p>Total: {total}</p>
+      {cosmetics.map((cosmetic) => (
+        <Cosmetic key={cosmetic.id} cosmetic={cosmetic}></Cosmetic>
+      ))}
     </div>
   );
 };
--- 01-Simple React/03-Modules and Data Storage/react-storage-and-handler/src/Components/Shoes/Shoes.js
+++ 01-Simple React/03-Modules and Data Storage/react-storage-and-handler/src/Components/Shoes/Shoes.js
@@ -1,14 +1,16 @@
 import React from "react";
-import { multiply } from "../../Utilities/calculate";
+import { add, multiply } from "../../Utilities/calculate";
 
 const Shoes = () => {
   const first = 13;
   const second = 11;
   const result = multiply(first, second);
+  const sum = add(first, second);
   return (
     <div>
       <h3>This is shoes component</h3>
       <p>Result : {result}</p>
+      <p>Sum: {sum}</p>
     </div>
   );
 };
