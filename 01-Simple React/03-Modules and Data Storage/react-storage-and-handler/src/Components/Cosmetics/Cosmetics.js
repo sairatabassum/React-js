0a13781eb4c9dea72c75b57a85a74c2f31c3593f
sairatabassum <sairatabassum386@gmail.com>
2023-03-30 05:01:26 +0600

    commit

--- 01-Simple React/03-Modules and Data Storage/react-storage-and-handler/src/Components/Cosmetics/Cosmetics.js
+++ 01-Simple React/03-Modules and Data Storage/react-storage-and-handler/src/Components/Cosmetics/Cosmetics.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { add } from "../../Utilities/calculate";
 import Cosmetic from "../Cosmetic/Cosmetic";
 // import add from "../../Utilities/calculate";
@@ -7,38 +7,44 @@ const Cosmetics = () => {
   const first = 55;
   const second = 66;
   const total = add(first, second);
-  const cosmetics = [
-    {
-      id: "6424a9ee9bf07614eda1eba5",
-      price: 99,
-      name: "Beryl Riley",
-    },
-    {
-      id: "6424a9ee9e1d64431657a7c3",
-      price: 600,
-      name: "Ernestine Rios",
-    },
-    {
-      id: "6424a9ee4150ef04ad2c764f",
-      price: 464,
-      name: "Houston Farrell",
-    },
-    {
-      id: "6424a9ee188543b5de4436c1",
-      price: 68,
-      name: "Knox Hernandez",
-    },
-    {
-      id: "6424a9eeb481060964705557",
-      price: 42,
-      name: "Gena Haney",
-    },
-    {
-      id: "6424a9eeda63d5679e7d7b98",
-      price: 203,
-      name: "Candace Davis",
-    },
-  ];
+  const [cosmetics, setCosmetics] = useState([]);
+  useEffect(() => {
+    fetch("data.json")
+      .then((res) => res.json())
+      .then((data) => setCosmetics(data));
+  }, []);
+  // const cosmetics = [
+  //   {
+  //     id: "6424a9ee9bf07614eda1eba5",
+  //     price: 99,
+  //     name: "Beryl Riley",
+  //   },
+  //   {
+  //     id: "6424a9ee9e1d64431657a7c3",
+  //     price: 600,
+  //     name: "Ernestine Rios",
+  //   },
+  //   {
+  //     id: "6424a9ee4150ef04ad2c764f",
+  //     price: 464,
+  //     name: "Houston Farrell",
+  //   },
+  //   {
+  //     id: "6424a9ee188543b5de4436c1",
+  //     price: 68,
+  //     name: "Knox Hernandez",
+  //   },
+  //   {
+  //     id: "6424a9eeb481060964705557",
+  //     price: 42,
+  //     name: "Gena Haney",
+  //   },
+  //   {
+  //     id: "6424a9eeda63d5679e7d7b98",
+  //     price: 203,
+  //     name: "Candace Davis",
+  //   },
+  // ];
   return (
     <div>
       <h1>Welcome to my cosmetics</h1>
