5293d635f52617919c355823df57d45c268a7d5b
sairatabassum <sairatabassum386@gmail.com>
2023-03-30 22:54:43 +0600

    commit

--- 01-Simple React/03-Modules and Data Storage/react-storage-and-handler/src/Utilities/fakedb.js
+++ 01-Simple React/03-Modules and Data Storage/react-storage-and-handler/src/Utilities/fakedb.js
@@ -1,14 +1,24 @@
 // use local storage to manage cart
 const addToDb = (id) => {
-  const quantity = localStorage.getItem(id);
+  let shoppingCart = {};
+
+  localStorage.setItem("shopping-cart", JSON.stringify(shoppingCart));
+
+  const quantity = shoppingCart[id];
   if (quantity) {
-    console.log("Already exist");
-    const newQuantity = Number(quantity) + 1;
-    localStorage.setItem(id, newQuantity);
+    const newQuantity = quantity + 1;
   } else {
-    console.log("New item");
-    localStorage.setItem(id, 1);
   }
+
+  //   const quantity = localStorage.getItem(id);
+  //   if (quantity) {
+  //     console.log("Already exist");
+  //     const newQuantity = Number(quantity) + 1;
+  //     localStorage.setItem(id, newQuantity);
+  //   } else {
+  //     console.log("New item");
+  //     localStorage.setItem(id, 1);
+  //   }
 };
 
 export { addToDb };
