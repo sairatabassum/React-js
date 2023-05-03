7bf0bec012fa9939a1dfb612a738b599b0a049b9
Saira Tabassum <sairatabassum386@gmail.com>
2023-05-03 14:58:10 +0600

    commit

--- 01-Simple React/04-react-practice/script.js
+++ 01-Simple React/04-react-practice/script.js
@@ -1,4 +1,4 @@
-const db = {};
+let db = {};
 
 const addToDb = (item) => {
   // Three ways to add a property to an object
@@ -6,10 +6,28 @@ const addToDb = (item) => {
   // db['alom']=1;
   // db[item] = 1;
 
+  const storedTracker = localStorage.getItem("checka-tracker");
+  if (storedTracker) {
+    // STRING TO OBJECT
+    db = JSON.parse(storedTracker);
+  }
+
   if (item in db) {
     db[item] = db[item] + 1;
   } else {
     db[item] = 1;
   }
+
+  localStorage.setItem("checka-tracker", JSON.stringify(db));
+
   console.log(db);
+};
+
+const removeItem = (item) => {
+  const storedTracker = localStorage.getItem("checka-tracker");
+  if (storedTracker) {
+    const storedObject = JSON.parse(storedTracker);
+    delete storedObject[item];
+    localStorage.setItem("checka-tracker", JSON.stringify(storedObject));
+  }
 };
