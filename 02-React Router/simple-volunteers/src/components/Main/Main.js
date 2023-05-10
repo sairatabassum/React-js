addb3d7b0640318b313e85e73dada7deec031827
Saira Tabassum <sairatabassum386@gmail.com>
2023-05-10 17:19:44 +0600

    useEffect dependency

--- 02-React Router/simple-volunteers/src/components/Main/Main.js
+++ 02-React Router/simple-volunteers/src/components/Main/Main.js
@@ -1,16 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Activity from "../Activity/Activity";
 import "./Main.css";
 import useVolunteers from "../../hooks/useVolunteers";
 
 const Main = () => {
-  const [data, setData] = useVolunteers();
+  // const [data, setData] = useVolunteers();
+  const [searchText, setSearchText] = useState("");
+  const [searchResult, setSearchResult] = useState([]);
+
+  useEffect(() => {
+    fetch("data.json")
+      .then((res) => res.json())
+      .then((data) => {
+        const match = data.filter((d) => d.title.includes(searchText));
+        setSearchResult(match);
+      });
+  }, [searchText]);
+
+  const handleSearchChange = (event) => {
+    setSearchText(event.target.value);
+  };
+  // const handleSearchChange2 = (event) => {
+  //   const searchText = event.target.value;
+  //   const match = data.filter((v) =>
+  //     v.title.toLowerCase().includes(searchText)
+  //   );
+  //   setSearchResult(match);
+  // };
 
   return (
     <div>
-      <h2>{data.length}</h2>
+      <h2>{searchResult.length}</h2>
+      <div style={{ margin: "20px" }}>
+        <input onChange={handleSearchChange} type="text" placeholder="search" />
+      </div>
       <div className="activity-container">
-        {data.map((activity) => (
+        {searchResult.map((activity) => (
           <Activity key={activity._id} activity={activity}></Activity>
         ))}
       </div>
