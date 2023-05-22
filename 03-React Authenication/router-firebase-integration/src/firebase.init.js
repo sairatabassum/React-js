c3d3a3e0cc3b6856bf2ffffd2084b18a7ca37882
Saira Tabassum <sairatabassum386@gmail.com>
2023-05-22 18:53:52 +0600

    commit

---
+++ 03-React Authenication/router-firebase-integration/src/firebase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAg_6p1OKruLLyZUcbp2m9nMred9VaofZE",
+  authDomain: "router-firebase-integrat-5d18a.firebaseapp.com",
+  projectId: "router-firebase-integrat-5d18a",
+  storageBucket: "router-firebase-integrat-5d18a.appspot.com",
+  messagingSenderId: "947100645985",
+  appId: "1:947100645985:web:11758dc1c4b5ec72770820",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
